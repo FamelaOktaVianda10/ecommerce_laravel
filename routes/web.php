cbd2ff91414b487798e49c7dbb2f7d4e6f51fc3d
LOQ <jinemoch@gmail.com>
2024-10-09 21:26:37 +0700

    CRUD

--- routes/web.php
+++ routes/web.php
@@ -7,34 +7,40 @@ use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\User\UserController;
 
 // Guest Route
-Route::group(['middleware' => 'guest'], function () {
+Route::group(['middleware' => 'guest'], function() {
     Route::get('/', function () {
         return view('welcome');
     });
-
     Route::get('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/post_register', [AuthController::class, 'post_register'])->name('post.register');
+    Route::post('/post-register',[AuthController::class, 'post_register'])->name('post.register');
 
-    Route::post('/post-login', [AuthController::class, 'login']);
-})->middleware('guest');
+    Route::post('/post-login', [AuthController::class, 'login'])
+        ->middleware('guest');
+});
 
 // Admin Route
-Route::group(['middleware' => 'admin'], function () {
+Route::group(['middleware' => 'admin'], function() {
     Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-
-    // Product Route
+    Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+    Route::post('/product/store', [ProductController::class, 'store'])->name('product.store');
+    Route::get('/admin/product/detail/{id}', [ProductController::class, 'detail'])->name('product.detail');
+    Route::get('/product/edit/{id}', [ProductController::class, 'edit'])->name('product.edit');
+    Route::post('/product/update/{id}', [ProductController::class, 'update'])->name('product.update');
+    Route::delete('/product/delete/{id}', [ProductController::class, 'delete'])->name('product.delete');
+    
+// Product Route
 Route::get('/product', [ProductController::class, 'index'])->name('admin.product');
-
-Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
-Route::post('/product/store', [ProductController::class, 'store'])->name('product.store');
-
-
-    Route::get('/admin-logout', [AuthController::class, 'adminLogout'])->name('admin.logout');
-})->middleware('admin');
+    Route::get('/admin-logout', [AuthController::class, 'admin_logout'])
+        ->name('admin.logout');
+});
 
 // User Route
-Route::group(['middleware' => 'web'], function () {
+Route::group(['middleware' => 'web'], function() {
     Route::get('/user', [UserController::class, 'index'])->name('user.dashboard');
 
-    Route::get('/user-logout', [AuthController::class, 'userLogout'])->name('user.logout');
+    Route::get('/user-logout', [AuthController::class, 'user_logout'])
+        ->name('user.logout');
 })->middleware('web');
+// Product Route
+Route::get('/user/product/detail/{id}', [UserController::class, 'detail_product'])->name('user.detail.product');
+Route::get('/product/purchase/{productId}/{userId}', [UserController::class,'purchase']);
